3a4c24b423390198e2fdb2511226feb0fc74cb5c
shg8 <stevengao2018@outlook.com>
2021-10-16 23:26:00 -0500

    Extension builds

--- EmbeddedClient/EmbeddedClient.h
+++ EmbeddedClient/EmbeddedClient.h
@@ -4,6 +4,16 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <CommonCrypto/CommonCrypto.h>
+
+#import "Try.h"
+
+#import "UIImageViewAligned.h"
+
+#import "HttpsEverywhereObjC.h"
+#import "NSData+GZIP.h"
+#import "NSFileManager+Tar.h"
 
 //! Project version number for EmbeddedClient.
 FOUNDATION_EXPORT double EmbeddedClientVersionNumber;
@@ -12,5 +22,4 @@ FOUNDATION_EXPORT double EmbeddedClientVersionNumber;
 FOUNDATION_EXPORT const unsigned char EmbeddedClientVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EmbeddedClient/PublicHeader.h>
-
 
