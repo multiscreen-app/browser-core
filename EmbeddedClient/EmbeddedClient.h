215a233890808b4b36c7e6f49ced945e109b158a
shg8 <stevengao2018@outlook.com>
2021-10-16 19:51:49 -0500

    Something

---
+++ EmbeddedClient/EmbeddedClient.h
@@ -0,0 +1,16 @@
+// Copyright 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmbeddedClient.
+FOUNDATION_EXPORT double EmbeddedClientVersionNumber;
+
+//! Project version string for EmbeddedClient.
+FOUNDATION_EXPORT const unsigned char EmbeddedClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmbeddedClient/PublicHeader.h>
+
+
