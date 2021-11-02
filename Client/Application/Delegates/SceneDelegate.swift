af74a78d7111873cfc8a3be66b42168b214601d4
Michał Buczek <michal.buczek@protonmail.com>
2021-11-02 15:27:02 +0100

    Fix #4244: Bump BraveCore library to 1.31.89. (#4447)

--- Client/Application/Delegates/SceneDelegate.swift
+++ Client/Application/Delegates/SceneDelegate.swift
@@ -348,8 +348,8 @@ extension SceneDelegate {
         let browserViewController = BrowserViewController(
             profile: profile,
             diskImageStore: diskImageStore,
-            historyAPI: appDelegate.braveCore.historyAPI!,
-            bookmarksAPI: appDelegate.braveCore.bookmarksAPI!,
+            historyAPI: appDelegate.braveCore.historyAPI,
+            bookmarksAPI: appDelegate.braveCore.bookmarksAPI,
             crashedLastSession: crashedLastSession)
         
         browserViewController.do {
