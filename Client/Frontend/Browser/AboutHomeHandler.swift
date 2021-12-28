brave/brave-core

Fix #4308, fix #4338: Improve session restore, update error pages (#4325)
Fix all session restore, reader-mode, error page, certificate error page, etc. bugs.
Used InternalURL and fix PrivilegedRequest.
Get rid of most of GCDWebServer code.

Client/Frontend/Browser/AboutHomeHandler.swift
@@ -2,40 +2,40 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import GCDWebServers
+import BraveUI
 
-/// Handles the page request to about/home/ so that the page loads and does not throw an error (404) on initialization
-struct AboutHomeHandler {
-    static func register(_ webServer: WebServer) {
-        webServer.registerHandlerForMethod("GET", module: "about", resource: "home") { (request: GCDWebServerRequest?) -> GCDWebServerResponse? in
-            return GCDWebServerResponse(statusCode: 200)
+class AboutHomeHandler: InternalSchemeResponse {
+    static let path = "about/home"
+
+    // Return a blank page, the webview delegate will look at the current URL and load the home panel based on that
+    func response(forRequest request: URLRequest) -> (URLResponse, Data)? {
+        guard let url = request.url else { return nil }
+        let response = InternalSchemeHandler.response(forUrl: url)
+        let bg = UIColor.braveBackground.toHexString()
+        // Blank page with a color matching the background of the panels which is displayed for a split-second until the panel shows.
+        let html = """
+            <!DOCTYPE html>
+            <html>
+              <body style='background-color:\(bg)'></body>
+            </html>
+        """
+        guard let data = html.data(using: .utf8) else {
+            return nil
         }
+        return (response, data)
     }
 }
 
-/// Handles the page request to about/license/ so that the page loads and does not throw an error (404) on initialization
-struct AboutLicenseHandler {
-    static func register(_ webServer: WebServer) {
-        webServer.registerHandlerForMethod("GET", module: "about", resource: "license") { (request: GCDWebServerRequest?) -> GCDWebServerResponse? in
-            let path = Bundle.main.path(forResource: "Licenses", ofType: "html")
-            do {
-                let html = try String(contentsOfFile: path!, encoding: .utf8)
-                return GCDWebServerDataResponse(html: html)
-            } catch {
-                print("Unable to register webserver \(error)")
-            }
-            return GCDWebServerResponse(statusCode: 200)
+class AboutLicenseHandler: InternalSchemeResponse {
+    static let path = "about/license"
+    
+    func response(forRequest request: URLRequest) -> (URLResponse, Data)? {
+        guard let url = request.url else { return nil }
+        let response = InternalSchemeHandler.response(forUrl: url)
+        guard let path = Bundle.main.path(forResource: "Licenses", ofType: "html"), let html = try? String(contentsOfFile: path, encoding: .utf8),
+            let data = html.data(using: .utf8) else {
+                return nil
         }
-    }
-}
-
-extension GCDWebServerDataResponse {
-    convenience init(XHTML: String) {
-        guard let data = XHTML.data(using: .utf8, allowLossyConversion: false) else {
-            assertionFailure("GCDWebServerDataResponse init, data is nil")
-            self.init()
-            return
-        }
-        self.init(data: data, contentType: "application/xhtml+xml; charset=utf-8")
+        return (response, data)
     }
 }

Client/Frontend/Browser/SessionRestoreHandler.swift
@@ -7,38 +7,65 @@ import WebKit
 import GCDWebServers
 import Shared
 
+private let apostropheEncoded = "%27"
+
+extension WKWebView {
+    // Use JS to redirect the page without adding a history entry
+    func replaceLocation(with url: URL) {
+        let safeUrl = url.absoluteString.replacingOccurrences(of: "'", with: apostropheEncoded)
+        evaluateSafeJavaScript(functionName: "location.replace", args: [safeUrl], sandboxed: true, escapeArgs: false, asFunction: true, completion: nil)
+        
+        evaluateJavaScript("location.replace('\(safeUrl)')", in: nil, in: .defaultClient, completionHandler: nil) //swiftlint:disable:this safe_javascript
+    }
+}
+
+func generateResponseThatRedirects(toUrl url: URL) -> (URLResponse, Data) {
+    var urlString: String
+    if InternalURL.isValid(url: url), let authUrl = InternalURL.authorize(url: url) {
+        urlString = authUrl.absoluteString
+    } else {
+        urlString = url.absoluteString
+    }
+
+    urlString = urlString.replacingOccurrences(of: "'", with: apostropheEncoded)
+    
+    let startTags = "<!DOCTYPE html><html><head><script>"
+    let endTags = "</script></head></html>"
+    let html = startTags + "location.replace('\(urlString)');" + endTags
+
+    let data = html.data(using: .utf8)!
+    let response = InternalSchemeHandler.response(forUrl: url)
+    return (response, data)
+}
+
 /// Handles requests to /about/sessionrestore to restore session history.
-struct SessionRestoreHandler {
-    static func register(_ webServer: WebServer) {
-        // Register the handler that accepts /about/sessionrestore?history=...&currentpage=... requests.
-        webServer.registerHandlerForMethod("GET", module: "about", resource: "sessionrestore") { request in
-            
-            // Session Restore should only ever be called from a privileged request
-            // This is because we need to inject the `RESTORE_TOKEN` into the page.
-            // Therefore, we don't want to leak it to a malicious page
-            guard let query = request?.query,
-                      query[PrivilegedRequest.key] == PrivilegedRequest.token else {
-                return GCDWebServerResponse(statusCode: 404)
-            }
-            
-            if let sessionRestorePath = Bundle.main.path(forResource: "SessionRestore", ofType: "html") {
-                do {
-                    var sessionRestoreString = try String(contentsOfFile: sessionRestorePath)
+class SessionRestoreHandler: InternalSchemeResponse {
+    static let path = "sessionrestore"
 
-                    defer {
-                        NotificationCenter.default.post(name: .didRestoreSession, object: self)
-                    }
+    func response(forRequest request: URLRequest) -> (URLResponse, Data)? {
+        guard let _url = request.url, let url = InternalURL(_url) else { return nil }
 
-                    let securityToken = UserScriptManager.messageHandlerToken.uuidString
-                    sessionRestoreString = sessionRestoreString.replacingOccurrences(of: "%SECURITY_TOKEN%", with: securityToken, options: .literal)
-                    sessionRestoreString = sessionRestoreString.replacingOccurrences(of: "%SESSION_RESTORE_KEY%", with: PrivilegedRequest.key, options: .literal)
-                    sessionRestoreString = sessionRestoreString.replacingOccurrences(of: "%SESSION_RESTORE_TOKEN%", with: PrivilegedRequest.token, options: .literal)
-
-                    return GCDWebServerDataResponse(html: sessionRestoreString)
-                } catch _ {}
-            }
-
-            return GCDWebServerResponse(statusCode: 404)
+        // Handle the 'url='query param
+        if let urlParam = url.extractedUrlParam {
+            return generateResponseThatRedirects(toUrl: urlParam)
         }
+
+        // From here on, handle 'history=' query param
+        let response = InternalSchemeHandler.response(forUrl: url.url)
+        guard let sessionRestorePath = Bundle.main.path(forResource: "SessionRestore", ofType: "html"),
+              var html = try? String(contentsOfFile: sessionRestorePath) else {
+            assert(false)
+            return nil
+        }
+        
+        html = html.replacingOccurrences(of: "%INSERT_UUID_VALUE%", with: InternalURL.uuid)
+        html = html.replacingOccurrences(of: "%security_token%", with: UserScriptManager.securityTokenString)
+        
+        guard let data = html.data(using: .utf8) else {
+            assert(false)
+            return nil
+        }
+
+        return (response, data)
     }
 }

multiscreen-app/browser-core

Client/Frontend/Browser/AboutHomeHandler.swift
@@ -2,40 +2,40 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import GCDWebServers
+import BraveUI
 
-/// Handles the page request to about/home/ so that the page loads and does not throw an error (404) on initialization
-struct AboutHomeHandler {
-    static func register(_ webServer: WebServer) {
-        webServer.registerHandlerForMethod("GET", module: "about", resource: "home") { (request: GCDWebServerRequest?) -> GCDWebServerResponse? in
-            return GCDWebServerResponse(statusCode: 200)
+class AboutHomeHandler: InternalSchemeResponse {
+    static let path = "about/home"
+
+    // Return a blank page, the webview delegate will look at the current URL and load the home panel based on that
+    func response(forRequest request: URLRequest) -> (URLResponse, Data)? {
+        guard let url = request.url else { return nil }
+        let response = InternalSchemeHandler.response(forUrl: url)
+        let bg = UIColor.braveBackground.toHexString()
+        // Blank page with a color matching the background of the panels which is displayed for a split-second until the panel shows.
+        let html = """
+            <!DOCTYPE html>
+            <html>
+              <body style='background-color:\(bg)'></body>
+            </html>
+        """
+        guard let data = html.data(using: .utf8) else {
+            return nil
         }
+        return (response, data)
     }
 }
 
-/// Handles the page request to about/license/ so that the page loads and does not throw an error (404) on initialization
-struct AboutLicenseHandler {
-    static func register(_ webServer: WebServer) {
-        webServer.registerHandlerForMethod("GET", module: "about", resource: "license") { (request: GCDWebServerRequest?) -> GCDWebServerResponse? in
-            let path = Bundle.embeddedMain.path(forResource: "Licenses", ofType: "html")
-            do {
-                let html = try String(contentsOfFile: path!, encoding: .utf8)
-                return GCDWebServerDataResponse(html: html)
-            } catch {
-                print("Unable to register webserver \(error)")
-            }
-            return GCDWebServerResponse(statusCode: 200)
+class AboutLicenseHandler: InternalSchemeResponse {
+    static let path = "about/license"
+    
+    func response(forRequest request: URLRequest) -> (URLResponse, Data)? {
+        guard let url = request.url else { return nil }
+        let response = InternalSchemeHandler.response(forUrl: url)
+        guard let path = Bundle.embeddedMain.path(forResource: "Licenses", ofType: "html"), let html = try? String(contentsOfFile: path, encoding: .utf8),
+            let data = html.data(using: .utf8) else {
+                return nil
         }
-    }
-}
-
-extension GCDWebServerDataResponse {
-    convenience init(XHTML: String) {
-        guard let data = XHTML.data(using: .utf8, allowLossyConversion: false) else {
-            assertionFailure("GCDWebServerDataResponse init, data is nil")
-            self.init()
-            return
-        }
-        self.init(data: data, contentType: "application/xhtml+xml; charset=utf-8")
+        return (response, data)
     }
 }

Client/Frontend/Browser/SessionRestoreHandler.swift
@@ -7,38 +7,65 @@ import WebKit
 import GCDWebServers
 import Shared
 
+private let apostropheEncoded = "%27"
+
+extension WKWebView {
+    // Use JS to redirect the page without adding a history entry
+    func replaceLocation(with url: URL) {
+        let safeUrl = url.absoluteString.replacingOccurrences(of: "'", with: apostropheEncoded)
+        evaluateSafeJavaScript(functionName: "location.replace", args: [safeUrl], sandboxed: true, escapeArgs: false, asFunction: true, completion: nil)
+        
+        evaluateJavaScript("location.replace('\(safeUrl)')", in: nil, in: .defaultClient, completionHandler: nil) //swiftlint:disable:this safe_javascript
+    }
+}
+
+func generateResponseThatRedirects(toUrl url: URL) -> (URLResponse, Data) {
+    var urlString: String
+    if InternalURL.isValid(url: url), let authUrl = InternalURL.authorize(url: url) {
+        urlString = authUrl.absoluteString
+    } else {
+        urlString = url.absoluteString
+    }
+
+    urlString = urlString.replacingOccurrences(of: "'", with: apostropheEncoded)
+    
+    let startTags = "<!DOCTYPE html><html><head><script>"
+    let endTags = "</script></head></html>"
+    let html = startTags + "location.replace('\(urlString)');" + endTags
+
+    let data = html.data(using: .utf8)!
+    let response = InternalSchemeHandler.response(forUrl: url)
+    return (response, data)
+}
+
 /// Handles requests to /about/sessionrestore to restore session history.
-struct SessionRestoreHandler {
-    static func register(_ webServer: WebServer) {
-        // Register the handler that accepts /about/sessionrestore?history=...&currentpage=... requests.
-        webServer.registerHandlerForMethod("GET", module: "about", resource: "sessionrestore") { request in
-            
-            // Session Restore should only ever be called from a privileged request
-            // This is because we need to inject the `RESTORE_TOKEN` into the page.
-            // Therefore, we don't want to leak it to a malicious page
-            guard let query = request?.query,
-                      query[PrivilegedRequest.key] == PrivilegedRequest.token else {
-                return GCDWebServerResponse(statusCode: 404)
-            }
-            
-            if let sessionRestorePath = Bundle.embeddedMain.path(forResource: "SessionRestore", ofType: "html") {
-                do {
-                    var sessionRestoreString = try String(contentsOfFile: sessionRestorePath)
+class SessionRestoreHandler: InternalSchemeResponse {
+    static let path = "sessionrestore"
 
-                    defer {
-                        NotificationCenter.default.post(name: .didRestoreSession, object: self)
-                    }
+    func response(forRequest request: URLRequest) -> (URLResponse, Data)? {
+        guard let _url = request.url, let url = InternalURL(_url) else { return nil }
 
-                    let securityToken = UserScriptManager.messageHandlerToken.uuidString
-                    sessionRestoreString = sessionRestoreString.replacingOccurrences(of: "%SECURITY_TOKEN%", with: securityToken, options: .literal)
-                    sessionRestoreString = sessionRestoreString.replacingOccurrences(of: "%SESSION_RESTORE_KEY%", with: PrivilegedRequest.key, options: .literal)
-                    sessionRestoreString = sessionRestoreString.replacingOccurrences(of: "%SESSION_RESTORE_TOKEN%", with: PrivilegedRequest.token, options: .literal)
-
-                    return GCDWebServerDataResponse(html: sessionRestoreString)
-                } catch _ {}
-            }
-
-            return GCDWebServerResponse(statusCode: 404)
+        // Handle the 'url='query param
+        if let urlParam = url.extractedUrlParam {
+            return generateResponseThatRedirects(toUrl: urlParam)
         }
+
+        // From here on, handle 'history=' query param
+        let response = InternalSchemeHandler.response(forUrl: url.url)
+        guard let sessionRestorePath = Bundle.embeddedMain.path(forResource: "SessionRestore", ofType: "html"),
+              var html = try? String(contentsOfFile: sessionRestorePath) else {
+            assert(false)
+            return nil
+        }
+        
+        html = html.replacingOccurrences(of: "%INSERT_UUID_VALUE%", with: InternalURL.uuid)
+        html = html.replacingOccurrences(of: "%security_token%", with: UserScriptManager.securityTokenString)
+        
+        guard let data = html.data(using: .utf8) else {
+            assert(false)
+            return nil
+        }
+
+        return (response, data)
     }
 }